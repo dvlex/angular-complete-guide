64ff28cd4d2392679cb53b6d225ead59c309af20
Alex Castillo <alex.castillo@razortracking.com>
2025-03-11 21:04:47 +0000

    add header, implement tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Agrega las rutas a tus archivos de plantilla
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
