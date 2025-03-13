d3d90ef60e3e0427593c3662e7d4ce038e7ebfbc
Alex Castillo <alex.castillo@razortracking.com>
2025-03-13 01:31:45 +0000

    work with signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  // selectedUser = DUMMY_USERS[randomIndex]; // without signal
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // with signal
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // with signal
 
+  /*get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar; // without signal
+  }*/
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomIndex]; // without signal
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); // with signal
+  }
 }
